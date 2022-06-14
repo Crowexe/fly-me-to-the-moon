6a6e08680c4b2249a26a1f236ebb1f28d7337eb0
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2022-06-14 13:19:19 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,6 +7,8 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
+
+
 const { REACT_APP_apiKey, REACT_APP_authDomain, REACT_APP_appId, REACT_APP_projectId, REACT_APP_storageBucket, REACT_APP_messagingSenderId } = process.env;
 
 const firebaseConfig = {
